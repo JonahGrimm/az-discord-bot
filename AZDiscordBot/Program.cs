19d916b9c85e7006b235b8406fbd689a7878e799
JonahGrimm <jonahmichaelgrimm@gmail.com>
2018-11-12 18:46:43 -0700

    Polish The Basic Files
    * Update the gitignore to this one: https://raw.githubusercontent.com/github/gitignore/master/VisualStudio.gitignore
    * Change README.md to have a basic description

--- AZDiscordBot/Program.cs
+++ AZDiscordBot/Program.cs
@@ -4,7 +4,7 @@ namespace AZDiscordBot
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
             Console.WriteLine("Hello World!");
         }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# az-discord-bot" 
+# AZ-Discord-Bot
+A bot custom made for the Arizona Rivals of Aether discord server.
+
